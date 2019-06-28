fc015ac3daf705145af687c697aad5a1d0e6b347
ly <doananhly1999@gmail.com>
2019-06-28 15:46:15 +0700

    [PHAN-37] 107 Tao ung dung ket hop React-Redux

--- src/App.js
+++ src/App.js
@@ -1,72 +1,16 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import News from './News';
 
 class App extends Component {
   render() {
-  var redux = require('redux');    
-  var oldState= {
-    num:["man hinh","chuot","ban phim"],
-    editStatus:true
-  }
-
-
-
-  var reducer1 = (state = oldState,action) => {
-    switch (action.type) {
-      case "CHANGE_EDIT_STATUS":
-        return {...state,editStatus:!state.editStatus}
-
-        case "ADD_NEW":
-        return {...state,num:[...state.num,action.newItem]}
-
-        case "DELETE":
-        return {...state,num:state.num.filter((value,i)=> i!== action.number)}
-
-    
-      default:
-          return state;
-
-    }
-    
-  }
-
-  var store1 = redux.createStore(reducer1);
-  store1.subscribe(()=>{
-    console.log(JSON.stringify(store1.getState()));
-  })
-  //console.log(store1.getState());
-  store1.dispatch({type:"CHANGE_EDIT_STATUS"})
-  //console.log(store1.getState());
-
-  store1.dispatch({
-    type:"ADD_NEW",
-    newItem:"Tai nghe"
-  })
-  //console.log(store1.getState());
-
-  store1.dispatch({
-    type:"DELETE",
-    number:0
-  })
-  //console.log(store1.getState());
-
     return (
       <div className="App">
-      <header className="App-header">
+      <header className="a1">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <News/>
     </div>
     );
   }
