f37182490e5694556a21a154066f5b0a6d1e192b
ly <doananhly1999@gmail.com>
2019-06-28 16:44:23 +0700

    [PHAN-38] 110 Cach goi ham trong Store tu component con

--- src/News.js
+++ src/News.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import editStatusReducer from './reducers/editStatusReducer';
 
 class News extends Component {
+    userEditStatusInstore = () =>{
+        var {dispatch} = this.props;
+        dispatch({type:'CHANGE_EDIT_STATUS'})
+    }
     render() {
         return (
             <div>
                 <h2>Day la Component News</h2>
+                <button onClick={() => this.userEditStatusInstore()}>Click di</button>
             </div>
         );
     }
 }
-
-export default News;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        editStatus: state.editStatus
+    }
+}
+export default connect(mapStateToProps)(News);
