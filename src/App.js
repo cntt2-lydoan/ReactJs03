9cca43b4c32c65c5353dc443feedc156a294deab
ly <doananhly1999@gmail.com>
2019-06-28 12:58:43 +0700

    [PHAN-34] 100 Cach viet tat ghi de va them moi toan tu ba cham

--- src/App.js
+++ src/App.js
@@ -14,12 +14,23 @@ class App extends Component {
     // console.log(p2);
     // console.log(p1);
 
-    var a1=[3,5,6,7];
-    var a2 = [...a1];
-    console.log(a1);
-    a2[0] = 100;
-    console.log(a1);
-    console.log(a2);
+    // var a1=[3,5,6,7];
+    // var a2 = [...a1];
+    // console.log(a1);
+    // a2[0] = 100;
+    // console.log(a1);
+    // console.log(a2);
+    var b1 ={
+      num: [34,56,23],
+      status:true
+    }
+    var b2 = {...b1,status:false};
+    console.log(b2);
+    var b3 = {...b1,num:[...b1.num,100]};
+    // b3.num[3]=100;
+    console.log(b3);
+
+    
 
     return (
       <div className="App">
