e81ca0be39305a8b223e377f1f16eee3ae5c7484
ly <doananhly1999@gmail.com>
2019-06-28 12:46:54 +0700

    [PHAN-34] 099 Su dung toan tu 3 cham

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    // var p1 = {
+    //   ten:"man hinh",
+    //   gia:90
+    // }
+    // console.log(p1);
+    // var p2 = {...p1} ;
+    // p2.gia=100;
+    // console.log(p2);
+    // console.log(p1);
+
+    var a1=[3,5,6,7];
+    var a2 = [...a1];
+    console.log(a1);
+    a2[0] = 100;
+    console.log(a1);
+    console.log(a2);
+
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,7 +38,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
